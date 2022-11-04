ad7bc44e5471b0e480605e9ba25491ec4898ce4d
molhot <sakata19991214@gmail.com>
2022-11-05 02:19:41 +0900

    test

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: satushi <sakata19991214@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 20:01:39 by satushi           #+#    #+#             */
-/*   Updated: 2022/10/16 17:41:30 by satushi          ###   ########.fr       */
+/*   Updated: 2022/11/05 02:19:27 by satushi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
+	if(lst == NULL)
+		return ;
 	if (!(lst == NULL) || !(new == NULL))
 	{
 		new->next = *lst;

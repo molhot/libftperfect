6965d55182cf96406eb28fd5bfae202780624da1
molhot <86003237+molhot@users.noreply.github.com>
2022-11-04 22:47:32 +0900

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -27,8 +27,6 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		s2 = s2 + 1;
 		n = n - 1;
 	}
-	if (n == 0)
-		return (0);
 	if ((n != 0) && (*s1 != *s2))
 		return ((unsigned char)*s1 - (unsigned char)*s2);
 	return (0);

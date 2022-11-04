27af21149c2f89b7d2b2ae49445cbdb68c89150f
molhot <sakata19991214@gmail.com>
2022-11-05 04:29:32 +0900

    nlasdj

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: satushi <sakata19991214@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 17:33:34 by satushi           #+#    #+#             */
-/*   Updated: 2022/11/05 04:01:32 by satushi          ###   ########.fr       */
+/*   Updated: 2022/11/05 04:29:24 by satushi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ size_t	ft_strlen(const char *string_row)
 	size_t	counter;
 
 	counter = 0;
-	while (string_row[counter] != '\0')
+	while (*string_row != '\0')
+	{
 		counter = counter + 1;
+		string_row = string_row + 1;
+	}
 	return (counter);
 }
